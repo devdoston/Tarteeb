924b50cfe9e99ba41460d4212a6a01910f69daa7
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-07-04 21:11:46 +0500

    ShouldThrowValidationExceptionOnAddIfEmailIsInvalidAndLogItAsync - > FAIL

--- Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
+++ Tarteeb.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
@@ -7,6 +7,7 @@ using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
+using Tarteeb.Api.Models.Foundations.Emails.Exceptions;
 using Tarteeb.Api.Models.Foundations.Users;
 using Tarteeb.Api.Models.Foundations.Users.Exceptions;
 using Xunit;
@@ -205,5 +206,53 @@ namespace Tarteeb.Api.Tests.Unit.Services.Foundations.Users
         //    this.loggingBrokerMock.VerifyNoOtherCalls();
         //    this.storageBrokerMock.VerifyNoOtherCalls();
         //}
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+
+        public async Task ShouldThrowValidationExceptionOnAddIfEmailIsInvalidAndLogItAsync(string? invalidText)
+        {
+            //given
+            var invalidEmail = invalidText;
+            User randomUser = CreateRandomUser();
+            User invalidUser = randomUser;
+            invalidUser.Email= invalidEmail;
+
+            var invalidUserException = new InvalidUserException();
+
+            invalidUserException.AddData(
+                key: nameof(User.Email),
+                values: "Email is required");
+
+            var expectedUserValidationException =
+                new UserValidationException(invalidUserException);
+
+            //when
+            ValueTask<User> addUserTask = this.userService.AddUserAsync(invalidUser);
+
+            UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(addUserTask.AsTask);
+
+            //then
+
+            actualUserValidationException.Should().BeEquivalentTo(expectedUserValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+               broker.GetCurrentDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(It.IsAny<User>()), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+
+        }
     }
 }
