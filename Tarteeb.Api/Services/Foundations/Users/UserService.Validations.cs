15d8ceee8e0604be8a120d8499fd46355ff6a06f
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-07-07 00:52:52 +0500

    CODE RUB: Cleaned

--- Tarteeb.Api/Services/Foundations/Users/UserService.Validations.cs
+++ Tarteeb.Api/Services/Foundations/Users/UserService.Validations.cs
@@ -4,9 +4,7 @@
 //=================================
 
 using System;
-using System.Data;
 using System.Text.RegularExpressions;
-using Microsoft.OData.Edm;
 using Tarteeb.Api.Models.Foundations.Users;
 using Tarteeb.Api.Models.Foundations.Users.Exceptions;
 
@@ -43,19 +41,6 @@ namespace Tarteeb.Api.Services.Foundations.Users
             Validate(
                 (Rule: IsInvalid(email), Parameter: nameof(User.Email)),
                 (Rule: IsValidEmail(email ?? ""), Parameter: nameof(User.Email)));
-        }
-
-        private static dynamic IsValidEmail(string email) => new 
-        {
-            Condition = IsNotEmail(email),
-            Message = "Email is not valid"
-        };
-
-        private static bool IsNotEmail(string email)
-        {
-            string regex = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$";
-
-            return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase) is false;
         }
 
         private void ValidateUserId(Guid userId) =>
@@ -160,6 +145,19 @@ namespace Tarteeb.Api.Services.Foundations.Users
                 Condition = firstDate != default && firstDate == secondDate,
                 Message = $"Date is same as {secondDateName}"
             };
+
+        private static dynamic IsValidEmail(string email) => new
+        {
+            Condition = IsNotEmail(email),
+            Message = "Email is not valid"
+        };
+
+        private static bool IsNotEmail(string email)
+        {
+            string regex = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$";
+
+            return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase) is false;
+        }
 
         private static void ValidateUserNotNull(User user)
         {
